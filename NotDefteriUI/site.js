610e2312dc1e6c35be1737d2332ccf8ff59500a0
Oguzhan Yusuf Dogan <oguzhanyusuf.dogan@gmail.com>
2024-02-26 11:11:18 +0300

    Kaydet, Sil

--- NotDefteriUI/site.js
+++ NotDefteriUI/site.js
@@ -2,61 +2,31 @@ const apiUrl = "http://localhost:5096/api/Notlar";
 const liste = document.getElementById("liste");
 const baslik = document.getElementById("baslik");
 const icerik = document.getElementById("icerik");
-const kaydet = document.getElementById("kaydet");
 const yeni = document.getElementById("yeni");
 const sil = document.getElementById("sil");
 const sag = document.getElementById("sag");
+const frmNot = document.getElementById("frmNot");
 let seciliNot = null;
 sag.hidden = true;
 
-yeni.addEventListener("click", function () {
-    axios.post(apiUrl, {
-        baslik: 'Yeni Not',
-        icerik: ''
-    })
-        .then(function (response) {
-            listele();
-            notGetir(response.data)
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-})
-
-kaydet.addEventListener("click", function () {
-    let activeItem = document.querySelector(".active");
-    let not;
-    axios.get(apiUrl + "/" + activeItem.id).then(res => {
-        not = res.data;
-    })
-    axios({
-        method: 'put',
-        url: apiUrl + "/" + activeItem.id,
-        data: {
-            id: activeItem.id,
-            baslik: baslik.value,
-            icerik: icerik.value
-        }
-    });
-    listele();
-})
-
-function listele() {
-    liste.innerHTML = "";
+function notlariCek() {
     axios.get(apiUrl).then(res => {
-        res.data.forEach(not => {
-            let li = document.createElement("li");
-            li.className += "list-group-item";
-            li.textContent = not.baslik;
-            li.id = not.id;
-            li.onclick = (e) => notGetir(not);
-            liste.appendChild(li);
-        });
-    })
+        notlar = res.data;
+        notlariListele();
+    });
 }
 
-listele();
+function notlariListele() {
+    liste.innerHTML = "";
+    notlar.forEach(not => {
+        let li = document.createElement("li");
+        li.className += "list-group-item";
+        li.textContent = not.baslik;
+        li.id = not.id;
+        li.onclick = (e) => notGetir(not);
+        liste.appendChild(li);
+    });
+}
 
 function notGetir(not) {
     seciliNot = not;
@@ -65,7 +35,6 @@ function notGetir(not) {
     icerik.value = not.icerik;
     secimiGuncelle();
 }
-
 
 function secimiGuncelle() {
     liste.childNodes.forEach(el => {
@@ -78,3 +47,49 @@ function secimiGuncelle() {
     });
 
 }
+
+yeni.addEventListener("click", function () {
+    axios.post(apiUrl, {
+        baslik: 'Yeni Not',
+        icerik: ''
+    })
+        .then(function (response) {
+            notlar.push(response.data);
+            notlariListele();
+            notGetir(response.data);
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+
+})
+
+frmNot.onsubmit = (e) => {
+    e.preventDefault();
+    let not = {
+        id: seciliNot.id,
+        baslik: baslik.value,
+        icerik: icerik.value
+    };
+
+    axios.put(apiUrl + "/" + not.id, not).then(res => {
+        seciliNot.baslik = baslik.value;
+        seciliNot.icerik = icerik.value;
+        notlariListele();
+        notGetir(not);
+    });
+};
+
+sil.onclick = (e) => {
+    axios.delete(apiUrl + "/" + seciliNot.id).then(res => {
+        let seciliIndex = notlar.indexOf(seciliNot);
+        notlar.splice(seciliIndex, 1);
+        notlariListele();
+        sag.hidden = true;
+        // Gösterecek not kalmadığında sağ taraf gizli kalacak. Not varsa silinenin olduğu yerdeki not seçili olacak, son eleman silindiyse de bir üstündeki seçili olacak.
+        if (notlar.length)
+            notGetir(notlar[Math.min(seciliIndex, notlar.length - 1)]);
+    });
+}
+
+notlariCek();
